11a0d75d326eef00a1ed0130d7413781c5944136
zee-ham-su <hsufiian@yahoo.com>
2024-01-25 11:47:02 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
-"""  a script that reads stdin
-line by line and computes metrics
-"""
+'''A script that reads stdin line by line and computes metrics'''
+
+
 import sys
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0,
-                404: 0, 405: 0, 500: 0}
 line_count = 0
 
 try:
     for line in sys.stdin:
-        line_count += 1
-        data = line.split()
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            line_count += 1
 
-        total_size += int(data[-1])
+        if line_count == 10:
+            line_count = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted((k, v)
+                                     for k, v in cache.items() if v != 0):
+                print('{}: {}'.format(key, value))
 
-        status_code = int(data[-2])
-        status_codes[status_code] += 1
 
-        if line_count % 10 == 0:
-            print("File size: {}".format(total_size))
-            for key in sorted(status_codes.keys()):
-                if status_codes[key] > 0:
-                    print("{}: {}".format(key, status_codes[key]))
+except Exception as err:
+    pass
 
-except Exception as e:
-    print(f"Error: {e}")
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted((k, v) for k, v in cache.items() if v != 0):
+        print('{}: {}'.format(key, value))
