ce2409a6a04661a17a13903ff321e66659a5997d
zee-ham-su <hsufiian@yahoo.com>
2024-02-13 08:08:09 -0800

    semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,27 +5,27 @@ const request = require('request');
 const movieId = process.argv[2];
 const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-function fetchCharactersAndPrint(namesList, idx) {
-    if (namesList.length === idx) {
-        return;
-    }
+function fetchCharactersAndPrint (namesList, idx) {
+  if (namesList.length === idx) {
+    return;
+  }
 
-    request(namesList[idx], (error, response, body) => {
-        if (error) {
-            console.error(error);
-        } else {
-            const characterName = JSON.parse(body).name;
-            console.log(characterName);
-            fetchCharactersAndPrint(namesList, idx + 1);
-        }
-    });
+  request(namesList[idx], (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const characterName = JSON.parse(body).name;
+      console.log(characterName);
+      fetchCharactersAndPrint(namesList, idx + 1);
+    }
+  });
 }
 
 request(movieUrl, (error, response, body) => {
-    if (error) {
-        console.error(error);
-    } else {
-        const charactersList = JSON.parse(body).characters;
-        fetchCharactersAndPrint(charactersList, 0);
-    }
+  if (error) {
+    console.error(error);
+  } else {
+    const charactersList = JSON.parse(body).characters;
+    fetchCharactersAndPrint(charactersList, 0);
+  }
 });
